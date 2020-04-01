406717a05ac029c58291217d62d0f38aaefcbb9a
Sawera-A14 <sawmirn03@gmail.com>
2020-03-31 21:50:40 -0700

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+$server = "localhost";
+$username = "root";
+$pass = "";
+$db = "users";
+
+$con = new mysqli($server,$username,$pass,$db);
+
+// to check connection
+
+// if($con)
+// {
+//     echo "connected";
+// }else
+// {
+//     echo "failed";
+// }
+
+?>
